b0220ae1c6ca0d770c5ac9d018380253fb5771d7
Kevin Lee <kevin.son.lee@gmail.com>
2019-02-17 11:57:59 -0600

    Updated files

--- nlp9.py
+++ nlp9.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import json
 import nltk
 import os
@@ -34,70 +33,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import json
-import nltk
-import os
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-
-nltk.download('stopwords')
-
-def main():
-    handle = '@goldenglobes'
-    handletext = handle[1:]
-    jsonData13 = json.loads(open('gg2013.json').read())
-    text13 = []
-    tokenized13 = []
-    official13 = []
-    punct13 = []
-    tokenizer = RegexpTokenizer(r'\w+')
-    stop_words = set(stopwords.words('english'))
-    stop_words.add('goldenglobes')
-
-    for item in jsonData13:
-        text13.append(item.get("text"))
-    
-    for tweet in text13:
-        punct13.append(nltk.wordpunct_tokenize(tweet))
-        if 'RT' not in tweet:
-            temp = tokenizer.tokenize(tweet)
-            if handletext == temp[0]:
-                official13.append(temp)
-        temp = tokenizer.tokenize(tweet)
-        tokenized13.append(temp)
-    
-    filtered13 = []
-    for tweet in official13:
-        filterTemp = []
-        for token in tweet:
-            if token not in stop_words:
-                filterTemp.append(token)
-        if(filterTemp):
-            filtered13.append(filterTemp)
-            
-    host13 = []
-    for tweet in filtered13:
-        hostTemp = []
-        if 'host' in tweet:
-            host13.append(tweet)
-        
-    tokenString = ""
-    tweetString = ""
-    for tweet in host13:
-        for token in tweet:
-            tokenString += token + ' '
-        tweetString += tokenString
-
-    #nameregex = re.compile(r'[A-Z][a-zA-Z]*')
-    potentialNames = nameregex.findall(tweetString)
-    #potentialNames
-    list([bigrams(word_tokenize(s)) for s in sent_tokenize(tweetString)])
-
-    #reader = TwitterCorpusReader(root='', fileids='gg2013.json')
-
-
-if __name__ == '__main__':
-    main()
-
->>>>>>> 4606e875e8492ae65e7762a7ec2cd91f78c34e0f
